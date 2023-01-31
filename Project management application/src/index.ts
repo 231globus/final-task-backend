4ccc9fbe70cc02d82b4d92d62dfebf93a6d0de68
Vladimir Kurganov <err0rfrost@gmail.com>
2023-02-01 01:11:10 +0600

    feat: config

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,10 +3,14 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = 'zavod';
+const Password = 'YGD8x5F99baVhXmf';
+const clasterInfo = 'cluster0.ltywpep';
+const applicationName = '';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}');
+    await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
